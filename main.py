0e5da26b6f91b6f7af29eca2591fb2ad8e64cc5f
Yernaz Kenesbekov <yernazkenesbekov24@gmail.com>
2026-02-27 14:25:11 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+while True:
+    text = input("Введите текст: ")
+    result = text.replace("<br>", "\n")
+    print("\nРезультат:")
+    print("=" * 30)
+    print("")
+    print(result)
+    print("")
+    print("=" * 30)
+    os.system(f"wl-copy '{result}'")
